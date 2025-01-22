8b6873ade0d47a2a017ac90571dd30f4606a7b81
mariocruz <pi.mariocruz@gmail.com>
2025-01-22 12:20:10 +0100

    Proyecto Render

---
+++ README.md
@@ -0,0 +1,19 @@
+```bash
+npm init -y
+npm i -D typescript tsx @types/node
+npx tsc -init
+npm i -D @types/express
+
+npm i -D @swc/core @swc/cli
+
+npm i prisma @prisma/client
+
+#Recomendable instalar la extension prisma
+
+npm i bcrypt
+npm i -D @types/bcrypt
+
+npm i jsonwebtoken
+npm i -D @types/jsonwebtoken
+
+```
---
+++ src/controlers/auth.controller.ts
@@ -0,0 +1,26 @@
+import { AuthService } from "@/sevices/auth.service";
+import express, { Response, Request } from 'express'
+
+export class AuthController {
+    static async register(req: Request, res: Response) {
+        try {
+            const userData = req.body
+            console.log(userData)
+            const newUser = await AuthService.register(userData)
+            res.status(201).json({ message: 'User register succesfully', newUser })
+        } catch (error) {
+            res.status(409).json({ message: 'Fallo al registrar el usuario' + error })
+        }
+    }
+    static async login(req: Request, res: Response) {
+
+        try {
+            const userData = req.body
+            console.log(userData)
+            const token = await AuthService.login(userData.email, userData.password)
+            res.status(201).json({ message: 'User register succesfully', token })
+        } catch (error) {
+            res.status(409).json({ message: 'Fallo al logear el usuario' + error })
+        }
+    }
+}
